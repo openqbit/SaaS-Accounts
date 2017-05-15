7126fcbf9007c2087668c869bc4562ff5489dc9b
testopenqubit <test@openqubit.com>
2017-05-15 19:21:49 +0100

    Master Copy

---
+++ OpenQbit.AccountsSystem.git/OpenQbit.Accounts.Service.WebAPIService/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OpenQbit.Accounts.Service.WebAPIService.Startup))]
+
+namespace OpenQbit.Accounts.Service.WebAPIService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
