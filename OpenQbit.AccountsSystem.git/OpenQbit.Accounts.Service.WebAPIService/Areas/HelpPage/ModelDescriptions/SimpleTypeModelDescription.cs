9c79cd2edebf3af88f8de4d8109a1d38e22e5793
Lilantha Ratapala <lilantha@openqubit.com>
2017-05-15 06:15:35 +0100

    Add files via upload

---
+++ OpenQbit.AccountsSystem.git/OpenQbit.Accounts.Service.WebAPIService/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OpenQbit.Accounts.Service.WebAPIService.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ OpenQbit.AccountsSystem.git/OpenQbit.Accounts.Service.WebAPIService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenQbit.Accounts.Service.WebAPIService.WebApiApplication" Language="C#" %>
