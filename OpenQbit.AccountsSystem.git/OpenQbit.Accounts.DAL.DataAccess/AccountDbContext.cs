1b9ae625e53d6289b1a1c6a145a5751bc7a2dc87
ChamikaAbeygunarathna <cpabeygunarathna@gmail.com>
2017-05-22 14:14:36 +0530

    Missing Files

---
+++ OpenQbit.AccountsSystem.git/OpenQbit.Accounts.DAL.DataAccess/AccountDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using OpenQubit.Account.Common.Models;
+//using ClassLibOpenQubit.Account.Common.Modelsrary1;
+
+namespace OpenQbit.Accounts.DAL.DataAccess
+{
+    class AccountDbContext : DbContext
+    {
+
+
+        public AccountDbContext() : base("Accounts") {
+        }
+        //public DbSet<Accounts> Accounts { get; set; }
+        //public DbSet<> Customer { get; set; }
+        //public DbSet<Transaction> Transaction { get; set; }
+        //public DbSet<ItemDetail> ItemDetails { get; set; }
+        //public DbSet<Item> Items { get; set; }
+
+            public DbSet<Account> Accounts { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Transaction> Transaction{ get; set; }
+        public DbSet<TransactionDetail> TransactionDetail { get; set; }
+        public DbSet<TransactionTemplate> TransactionTemplate { get; set; }
+        public DbSet<TransactionTemplateDetail> TransactionTemplateDetail { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
