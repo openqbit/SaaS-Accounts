66d71183e36b6cd4da1a3b1033483197378a03f3
Sandun Prasanga <Sandun Prasanga@SANDUNPRASANGA>
2017-06-23 03:26:11 +0530

    UtilCreated

---
+++ OpenQbit.AccountsSystem.git/OpenQubit.Account.Common.Ioc/UnityResolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace OpenQubit.Account.Common.Ioc
+{
+    public class UnityResolver
+    {
+        private static readonly IUnityContainer Container = new UnityContainer();
+
+        private static void Register()
+        {
+            Container.RegisterType<IBranchResourceAllocationManager, BranchResourceAllocationManager>();
+            Container.RegisterType<IBatchManager, BatchManager>();
+            Container.RegisterType<IBatchEnrolmentManager, BatchEnrolmentManager>();
+            Container.RegisterType<IBranchManager, BranchManager>();
+            Container.RegisterType<IEmployeeManager, EmployeeManager>();
+            Container.RegisterType<IInstituteManager, InstituteManager>();
+            Container.RegisterType<IRepository, Repository>();
+            Container.RegisterType<ILogger, Logger>();
+        }
+    }
+}
